a221e8ebe8bc1df665505c2a80dfbec8adb5b00d
Yorick Devleeschouwer <yorick.devleeschouwer@gmail.com>
2023-12-10 23:10:15 +0100

    Feat: Enable cors + Mongo setup + link routes

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
-const express = require('express');
+const fs = require('fs');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+//connect to mongodb
+const credentials = "/etc/secrets/credentials.pem";
+mongoose.connect("mongodb+srv://lab6.gfpqmsa.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority", {
+    tlsCertificateKeyFile: credentials,
 });
 
+//check connection 
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Connected to MongoDB!");
+//import routes
+
+
+const scoreRouter = require("./routes/api/v1/scores");
+
+app.use(express.json());
+
+//use routes
+app.use("/api/v1/scores", scoreRouter);
+
+
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
+});
+// enable cors express
+const cors = require("cors");
+app.use(cors());
